1ea63c8ad66e1e9227d33567d4b8a42ce6f94b06
Yurii-Shutkin <yurashutkinn@gmail.com>
2025-11-12 14:37:19 +0200

    fixed swiper resize viewport update problem

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,55 @@
-const swiper = new Swiper('.swiper', {
-  // observer: true,
-  // observeParents: true,
-  // observeSlideChildren: true,
-})
+// let nftCardsSwiper;
+// const initNftCardsSwiper = () => {
+//   if (nftCardsSwiper) nftCardsSwiper.destroy(true, true);
 
-let nftCardsSwiper;
-const initNftCardsSwiper = () => {
-  if (nftCardsSwiper) nftCardsSwiper.destroy(true, true);
+//   nftCardsSwiper = new Swiper('.nft-cards__swiper', {
+//   slidesPerView: 1.2,
+//   freeMode: false,
+//   spaceBetween: 30,
+//   watchOverflow: true,
+//   grabCursor: true,
+//   observer: true,
+//   observeParents: true,
+//   observeSlideChildren: true,
+//   scrollbar: {
+//       el: '.swiper-scrollbar',
+//       draggable: true,
+//   },
 
-  nftCardsSwiper = new Swiper('.nft-cards__swiper', {
+//   breakpoints: {
+//     320: {
+//       slidesOffsetBefore: 20,
+//     },
+//     375: {
+//       slidesOffsetBefore: 20,
+//       spaceBetween: 10,
+//     },
+//     576: {
+//       slidesPerView: 1.7,
+//     },
+//     577: {
+//       slidesPerView: 2.2,
+//     },
+
+//     769: {
+//       slidesPerView: 1.8,
+//       slidesOffsetBefore: 20,
+//     },
+//     1320: {
+//       slidesPerView:2.5,
+//       slidesOffsetBefore: 0,
+//     },
+
+//     1440: {
+//       slidesPerView: 3,
+//       slidesOffsetBefore: 0,
+//       spaceBetween: 30,
+//     },
+//   },
+// })
+// }
+
+const nftCardsSwiper = new Swiper('.nft-cards__swiper', {
   slidesPerView: 1.2,
   freeMode: false,
   spaceBetween: 30,
@@ -17,6 +58,7 @@ const initNftCardsSwiper = () => {
   observer: true,
   observeParents: true,
   observeSlideChildren: true,
+  updateOnWindowResize: true,
   scrollbar: {
       el: '.swiper-scrollbar',
       draggable: true,
@@ -24,9 +66,13 @@ const initNftCardsSwiper = () => {
 
   breakpoints: {
     320: {
+      slidesPerView: 1.2,
+
       slidesOffsetBefore: 20,
     },
     375: {
+      slidesPerView: 1.2,
+
       slidesOffsetBefore: 20,
       spaceBetween: 10,
     },
@@ -53,16 +99,12 @@ const initNftCardsSwiper = () => {
     },
   },
 })
-}
 
 const collectionSwiper = new Swiper('.collections__swiper', {
   slidesPerView: 1.2,
   freeMode: false,
   grabCursor: true,
   spaceBetween: 30,
-  // observer: true,
-  // observeParents: true,
-  // observeSlideChildren: true,
   scrollbar: {
       el: '.swiper-scrollbar',
       draggable: true,
@@ -90,10 +132,10 @@ const collectionSwiper = new Swiper('.collections__swiper', {
   },
 })
 
-initNftCardsSwiper();
+// initNftCardsSwiper();
 
-let resizeTimeout;
-window.addEventListener('resize', () => {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(initNftCardsSwiper, 300); 
-});
+// let resizeTimeout;
+// window.addEventListener('resize', () => {
+//   clearTimeout(resizeTimeout);
+//   resizeTimeout = setTimeout(initNftCardsSwiper, 300); 
+// });
